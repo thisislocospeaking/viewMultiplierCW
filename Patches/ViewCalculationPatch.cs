abdb1a42eb21698e8d54fa19ab3ebf7c2982e6f3
thisislocospeaking <kalezicdominik@gmail.com>
2024-04-06 23:01:50 +0100

    Add files

---
+++ Patches/ViewCalculationPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+namespace ViewMultiplier.Patches
+{
+    [HarmonyPatch(typeof(ContentBuffer))]
+    internal class ViewCalculationPatch
+    {
+        internal static float MultiplierValue
+        {
+            get { return (float)Plugin.ViewMultiplierCFG?.Bind("Settings", "ViewMultiplier", 2, "Multiplies views by this integer. Default value is 2.").Value; }
+        }
+
+        [HarmonyPatch(nameof(ContentBuffer.GetScore))]
+        [HarmonyPostfix]
+        static void MultiplierPatch(ref float __result)
+        {
+            float multVal = MultiplierValue;
+            __result *= multVal;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ViewMultiplier.Patches;
+
+namespace ViewMultiplier;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+
+    public static ConfigFile? ViewMultiplierCFG;
+    public static ManualLogSource? VMLogger;
+
+    private void Awake()
+    {
+        VMLogger = base.Logger;
+        VMLogger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        
+        ViewMultiplierCFG = base.Config;
+        ViewMultiplierCFG?.Bind("Settings", "ViewMultiplier", 2, "Multiplies views by this integer. Default value is 2.");
+
+        VMLogger.LogInfo("Config file is loaded!");
+
+        harmony.PatchAll(typeof(Plugin));
+        harmony.PatchAll(typeof(ViewCalculationPatch));
+    }
+}
